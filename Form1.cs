7ef14e46bdb432d84646e9ae541e29f32aba5ec7
blaqqqqq <127648279+blaqqqqq@users.noreply.github.com>
2023-03-20 03:20:35 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace codetkhome3
+{
+    public partial class mainform : Form
+    {
+        public mainform()
+        {
+            InitializeComponent();
+        }
+
+        private void nextform_button_Click(object sender, EventArgs e)
+        {
+            SecondWindform form2 = new SecondWindform();
+            form2.Show();
+            nextform_button.Enabled = false;
+            if (true_cek.Checked == true && nextform_button.Enabled == false)
+            {
+                enter.Enabled = true;
+            }
+        }
+
+        private void true_cek_CheckedChanged(object sender, EventArgs e)
+        {
+            if (true_cek.Checked == true && true_cek.Enabled == false)
+            {
+                enter.Enabled = true;
+            }
+            if (true_cek.Checked == false && nextform_button.Enabled == false)
+            {
+                enter.Enabled = false;
+            }
+
+        }
+
+        private void enter_Click(object sender, EventArgs e)
+        {
+            SecondWindform form2 = new SecondWindform();
+            if (true_cek.Checked == true && nextform_button.Enabled == false)
+            {
+                enter.Enabled = true;
+            }
+            string nama = Convert.ToString(nama_text.Text);
+            if (nama_text.Text == "")
+            {
+                MessageBox.Show("Enter the correct Input");
+            }
+            if (artist.Text == "")
+            {
+                MessageBox.Show("Enter the correct Input");
+            }
+            string favrtist = Convert.ToString(artist.Text);
+            form2.Show();
+        }
+        public void background(Color color)
+        {
+            this.BackColor = color;
+        }
+
+    }
+}
+
---
+++ Form2.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace codetkhome3
+{
+    public partial class SecondWindform : Form
+    {
+        public SecondWindform()
+        {
+            InitializeComponent();
+        }
+        public void labeltext(string name, string artist)
+        {
+            labeltxt.Text = "Hi, my name is " + name + "and my favorite artist is" + artist;
+        }
+
+        private void magic_Click(object sender, EventArgs e)
+        {
+            bool check = false;
+            if (agre.Checked == true && yes.Checked == true)
+            {
+                magic.Enabled = true;
+            }
+            mainform form1 = new mainform();
+            if (red.Checked == true)
+            {
+                form1.BackColor = Color.Red;
+                check = true;
+            }
+            else if (green.Checked == true)
+            {
+                form1.BackColor = Color.Green;
+                check = true;
+            }
+            else if (blue.Checked == true)
+            {
+                form1.BackColor = Color.Blue;
+                check = true;
+            }
+            else if (yellow.Checked == true)
+            {
+                form1.BackColor = Color.Yellow;
+                check = true;
+            }
+            if (pink.Checked == true)
+            {
+                form1.ForeColor = Color.Pink;
+                check = true;
+            }
+            else if (orange.Checked == true)
+            {
+                form1.ForeColor = Color.Purple;
+                check = true;
+            }
+            else if (black.Checked == true)
+            {
+                form1.ForeColor = Color.Orange;
+                check = true;
+            }
+            if (check == false)
+            {
+                MessageBox.Show("choose the color");
+            }
+
+            form1.Show();
+        }
+
+        private void agre_CheckedChanged(object sender, EventArgs e)
+        {
+            if (agre.Checked == true && yes.Checked == true)
+            {
+                magic.Enabled = true;
+            }
+        }
+
+        private void yes_CheckedChanged(object sender, EventArgs e)
+        {
+
+            if (agre.Checked == true && yes.Checked == true)
+            {
+                magic.Enabled = true;
+            }
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
